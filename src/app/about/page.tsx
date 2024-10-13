bc3bb4af5214cf63d1e65d1bd0c4a2285875cc0e
Mehwsish-Raisi <raisimish@gmail.com>
2024-10-13 20:06:10 +0500

    class assignment

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "../components/Header/header"
+const AboutPage = () => {
+  return (
+    <div>
+        
+      <h1> This is about page</h1>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default AboutPage;
---
+++ src/app/components/Hero/hero.tsx
@@ -0,0 +1,13 @@
+
+import style from "./hero.module.css"
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h2 className={style.css}>This is a Hero Section</h2>
+            <br />
+            <p>Mastering TypeScript, now diving into Next.js to build faster, scalable web applications. Learning Next.js can transform how you build web applications by offering a more efficient, scalable, and high-performance development experience. Next.js is built on top of React, so it extends React's capabilities by offering additional features that make development faster and more efficient.</p>
+        </div>
+    )
+}
+export default HeroPage;
